18b56fd8443b8c815fd653d0becf386270efe04e
Hrushikesh1430 <hdtawde7@gmail.com>
2022-04-29 12:12:52 +0530

    update

--- live.php
+++ live.php
@@ -143,45 +143,7 @@ $current_date = strtotime(date("r"));
         <!-- mobile view -->
         <div class="d-lg-none position-relative">
             <!-- Enter Code for Mobile UI here -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand p-3 pt-4" href="#"> <img src="assets/images/f_logo.png"></a>
-                <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav p-4">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Events</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Gallery</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Community</a>
-                        </li>
-                        <!-- <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown link
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li> -->
-                    </ul>
-                </div>
-            </nav>
+            <div class="mobile-header"></div>
 
             <div class="container-fluid live-section1" style="padding-top:150px;padding-bottom: 100px;">
                 <h2 class="text-center pb-5"><span style="color: #FFFFFF;">LIVE</span> EVENTS</h2>
@@ -256,6 +218,7 @@ $current_date = strtotime(date("r"));
         $(document).ready(function() {
             $(".header").load('template/header.php');
             $(".footer-top").load('template/footer.html');
+            $(".mobile-header").load('template/mobile-header.html');
 
             $(window).on('scroll', function() {
                 var scroll = $(window).scrollTop();
